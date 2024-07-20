80c93d7bd85cf2f8aab78a022a3b20dd4a632bef
DifAnon <142886715+DifAnon@users.noreply.github.com>
2024-07-20 23:50:02 +0300

    Add files via upload

---
+++ random_img.js
@@ -0,0 +1,8 @@
+sha = function(){
+return Math.floor(Math.random() * 101); 
+if(sha = 0){
+	return 10
+}
+}
+
+document.getElementById("yobaimg").innerHTML = '<img src="yobas/yoba'+sha()+'.jpg" style="max-width:100%; max-height:100%;"/>';
