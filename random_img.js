1154bb553fb44121d505b25d409971aa3b0f7e66
DifAnon <142886715+DifAnon@users.noreply.github.com>
2024-07-20 23:55:21 +0300

    Update random_img.js

--- random_img.js
+++ random_img.js
@@ -1,8 +1,5 @@
 sha = function(){
-return Math.floor(Math.random() * 101); 
-if(sha = 0){
-	return 10
-}
+return Math.floor(Math.random() * 102); 
 }
 
 document.getElementById("yobaimg").innerHTML = '<img src="yobas/yoba'+sha()+'.jpg" style="max-width:100%; max-height:100%;"/>';
